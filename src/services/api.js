e0ad09a35a4d8855341740b4d73b2e2760009858
nikkolast88 <nikkolast88@gmail.com>
2024-02-18 11:34:35 +0800

    chore: update

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { vFetch } from '~/utils'
 /**
  *
  * @param {string} params
- * @returns {Promise<import('~/types').FetchResponse<string>>}
+ * @returns {Promise<import('~/types').FetchResponse<string>>}.
  */
 export function postLamps(params) {
   return vFetch('/login/getVerifyImg', params)
